7d517f13724ccab6de2bfe218309340060d09db1
Juan Carlos Ricalde Poveda <jucaripo@hotmail.co>
2015-01-23 09:16:39 -0600

    Initial Commit

---
+++ metalNsCoders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  metalNsCoders
+//
+//  Created by Juan Carlos Ricalde Poveda on 23/01/15.
+//  Copyright (c) 2015 jucaripo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
