42e88e4aa3c59ecad39cf7f5591b088db485ef6c
Juan Carlos Ricalde Poveda <jucaripo@hotmail.co>
2015-05-14 09:30:31 -0500

    Código de ejemplo de como usar OpenGL-ES del curso de introducción a metal y openGL-ES usando swift

--- metalNsCoders/ViewController.swift
+++ metalNsCoders/ViewController.swift
@@ -8,17 +8,61 @@
 
 import UIKit
 
+// Se requiere de la libreria  SceneKit para poder trabajar con Metal.
+import SceneKit
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Codigo para trabajar con Metal
+        
+        // Crear objeto vista
+        let sceneView = SCNView( frame: view.frame)
+       
+        
+        
+        
+        // Crear escena
+        let scene = SCNScene()
+        // asignar escena a la vista
+        sceneView.scene = scene
+        
+        // configurar Luces , camara y estadisticas
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.allowsCameraControl = true
+        sceneView.showsStatistics = true
+        
+        // crear cajita
+        let box = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 0)
+        
+        // asignar nodo a escena
+        let boxNode = SCNNode(geometry: box)
+        boxNode.position = SCNVector3(x: 250, y: 150, z: 460)
+        scene.rootNode.addChildNode(boxNode)
+        
+        
+        // luces 
+        let light = SCNLight()
+        light.color = UIColor.redColor()
+        let lightNode = SCNNode()
+        lightNode.light = light
+        // posicion de la lampara
+        lightNode.position = SCNVector3(x: -30, y: 30, z: 60)
+        // agregamos el nodo de luz a la escena
+        scene.rootNode.addChildNode(lightNode)
+        
+        
+        // mostrar la vista en el dispositivo
+        view.addSubview(sceneView)
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
